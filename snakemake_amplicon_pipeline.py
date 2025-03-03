fa9c6aa960fe9e036ef76354d222290f071a22f7
kaboyo <olimicls2015@gmail.com>
2025-03-03 17:17:33 +0000

    changes

--- snakemake_amplicon_pipeline.py
+++ snakemake_amplicon_pipeline.py
@@ -6,7 +6,7 @@ SRA,FRR = globe_wildcards (rawReads/(sra)_[frr].fastq.gz)
 
 rule all:
     input: 
-        expand("rawQC/{sra}_{frr}_fastqc.{extension}", sra=SRA)
+        expand("rawQC/{sra}_{frr}_fastqc.{extension}", sra=SRA), frr=FRR, extension=["html", "zip"])
 
      # TODO: add your rules for other steps here
 rule rawFastqc:
@@ -16,19 +16,31 @@ rule rawFastqc:
             zip="rawFastqc/ {sra}_{ffr}_fastqc.zip",
             html="rawFastqc/{sra}_{frr}_fastqc_report.html"
             threads:
+                2
+                params:
+                    path="rawFastqc/"
+                    
                 shell:
                 """
-                fastqc -o results/fastqc -t {threads} {input}
+                fastq {input.rawread} --threads {threads}  -o {params.path}
                 """
 
     rule trimmomaticd:
     input:
-        rawReads = "rawReads/(sra)_[frr].fastq.gz",
-        output: f"{TRIMMED_DIR}/{sample}_R1_trimmed.fastq",
+        read1= "rawReads/(ids)_1.fastq.gz",
+        read2= "rawReads/(ids)_2.fastq.gz"
+        
+        output: 
+            forwardpaired="trimmedReads/{ids}_R1_trimmed.fastq",
+            reversepaired="trimmedReads/{ids}_R2_trimmed.fastq",
+
             threads: 
+                4
+                params:
+                    path="trimmedReads/"
                 shell: 
                 """
-                java -jar /path/to/Trimmomatic-0.39.jar PE -threads {threads} -phred33 {input.fq1} {input.fq2} {output.trimmed_fq1} {output.unpaired_fq1} {output.trimmed_fq2} {output.unpaired_fq2} ILLUMINACLIP:/path/to/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+                trimmomatic PE -threads {threads} {input.read1} {input.read2} o {output.trimmed_fq1} {output.unpaired_fq1} {output.trimmed_fq2} {output.unpaired_fq2} ILLUMINACLIP:/path/to/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
                 """
     rule cutadapter:
         input:
